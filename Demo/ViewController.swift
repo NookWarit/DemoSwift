6f5abc28b8aca54c69049360d84907b039118282
warittha <nookzzy@tuktukpass.com>
2019-09-05 15:19:36 +0700

    navigation

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -1,13 +1,15 @@
 import UIKit
 import Alamofire
 import SwiftyJSON
+import NVActivityIndicatorView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , NVActivityIndicatorViewable {
     //Mark: - IBOutlet
     @IBOutlet weak var tableView : UITableView!
     
     //Mark: -Variable
     let displayCell = "DisplayCell"
+    var venues: [Venue] = []
     
     
     //Mark: - Life Cycle
@@ -16,16 +18,19 @@ class ViewController: UIViewController {
         
         tableView.register(UINib(nibName: "DisplayCell", bundle: nil), forCellReuseIdentifier: displayCell)
         
+        let size = CGSize(width: 42, height: 42)
+        startAnimating(size, message: "",
+                       type: .ballTrianglePath, fadeInAnimation: nil)
         Alamofire.request("https://api.foursquare.com/v2/venues/explore?near=NYC&oauth_token=D1LRRCXBCXHHWWLQ4O41QA0NJSMSMYOR5IJLMT4IUV3HCKDZ&v=20180815",
-                          
                           method: .get)
-            .responseJSON(completionHandler: {response in
+            .responseJSON(completionHandler: { response in
                 let res = JSON(response.value ?? [:])
                 let group = res["response"]["groups"].arrayValue.first?.dictionaryValue
                 let items = group?["items"]?.arrayValue
-
-
-
+                self.venues = items?.map({ Venue(response: $0["venue"]) }) ?? []
+                self.tableView.reloadData()
+                self.stopAnimating()
+                
 //                let res = response.value as? Dictionary<String,Any>
 //                let ress = res?["response"] as? Dictionary<String,Any>
 //                let group = ress?["groups"] as? Array<Any>
@@ -40,13 +45,16 @@ class ViewController: UIViewController {
 //Mark: - UITableViewDatasorce & Delegate
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return venues.count
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
+        return UITableView.automaticDimension
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: displayCell, for: indexPath) as! DisplayCell
+        
+        let data = venues[indexPath.row]
+        cell.configureCell(icon: data.catIcon, name: data.name, address: data.location)
         
         return cell
     }
@@ -55,6 +63,16 @@ extension ViewController: UITableViewDataSource{
 extension ViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let SBMain = UIStoryboard(name: "Main", bundle: nil)
+        let vc = SBMain.instantiateViewController(withIdentifier: "Detail") as! DetailViewController
+        let data = venues[indexPath.row]
+        vc.icon = data.catIcon
+        vc.name = data.name
+        vc.address = data.location
+        navigationController?.pushViewController(vc, animated: true)
+        
+        
     }
 }
 
